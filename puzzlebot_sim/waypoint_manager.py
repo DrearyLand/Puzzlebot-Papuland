bc0be155d84d7dd3295e38a5f98de9b536b96427
JPCN33 <A01748818@tec.mx>
2026-06-01 19:39:57 -0600

    Waypoint updated

--- puzzlebot_sim/waypoint_manager.py
+++ puzzlebot_sim/waypoint_manager.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from geometry_msgs.ms#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
 from geometry_msgs.msg import Pose2D
 from nav_msgs.msg import Odometry
-from rclpy.qos import QoSProfile,DurabilityPolicy
 import math
 
 class WaypointManager(Node):
     def __init__(self):
         super().__init__('waypoint_manager')
-        latched = QoSProfile(depth=1)
-        latched.durability = DurabilityPolicy.TRANSIENT_LOCAL
-        self.publisher_ = self.create_publisher(Pose2D, 'goal', latched)
+        
+        self.publisher_ = self.create_publisher(Pose2D, 'goal', 10)
         self.subscription = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
         
-        # RUTA DEL LABERINTO
+        # RUTA DEL LABERINTO (Un solo destino final)
         # Formato: (X, Y)
         self.waypoints = [
-            (0.30, -1.20),  # X1: Primer pasillo, abajo al centro (Cerca del ArUco 708/F)
-            (0.90, -1.50),  # X2: Centro del laberinto (Debajo del ArUco 706/C)
-            (0.60, -2.70),  # X3: Pasillo del extremo derecho (Cerca del ArUco 75/G)
-            (2.70, -0.60),  # X4: Salida superior izquierda (Cerca del ArUco 701/H)
+            (2.70, -0.60),  # Meta única: Salida superior izquierda (Cerca de ArUco H)
         ]
         
         self.current_index = 0
-        self.goal_tolerance = 0.20 # Tolerancia ampliada a 20cm para la vida real
+        self.goal_tolerance = 0.20 # Tolerancia de 20cm
         self.goal_published = False
 
     def odom_callback(self, msg):
@@ -38,7 +36,7 @@ class WaypointManager(Node):
         dist = math.sqrt((target_x - x)**2 + (target_y - y)**2)
 
         if dist < self.goal_tolerance:
-            self.get_logger().info(f'¡Marca X{self.current_index + 1} alcanzada!')
+            self.get_logger().info('¡Meta final alcanzada con éxito!')
             self.current_index += 1
             self.goal_published = False
 
@@ -47,7 +45,7 @@ class WaypointManager(Node):
             goal_msg.x = self.waypoints[self.current_index][0]
             goal_msg.y = self.waypoints[self.current_index][1]
             self.publisher_.publish(goal_msg)
-            self.get_logger().info(f'>>> Imán activado hacia X{self.current_index + 1}: X={goal_msg.x}, Y={goal_msg.y}')
+            self.get_logger().info(f'>>> Imán activado hacia la meta única: X={goal_msg.x}, Y={goal_msg.y}')
             self.goal_published = True
 
 def main(args=None):
